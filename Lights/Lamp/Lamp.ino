c4540cefcfd2925605cb8f1d957d54a000e7a28e
rorikmelberg <rorik.melberg@gmail.com>
2021-12-12 11:46:25 -0800

    Update Lamp.ino

--- Lights/Lamp/Lamp.ino
+++ Lights/Lamp/Lamp.ino
@@ -7,7 +7,7 @@
 #define PUSHBUTTON_PIN 2
 
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 59
+#define LED_COUNT 120
 #define MAX_HUE 65535
 
 // Declare our NeoPixel strip object:
